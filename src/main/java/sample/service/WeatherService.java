379ade85cf13597f1cd109287e3522c0a6c8a616
Tomek <tomusio@vp.pl>
2016-11-17 17:57:33 +0100

    working in progress

---
+++ src/main/java/sample/service/WeatherService.java
@@ -0,0 +1,11 @@
+package sample.service;
+
+import sample.model.Weather;
+
+/**
+ * Created by Tomek on 2016-11-17.
+ */
+public interface WeatherService {
+    Weather getWeatherFromUrl(String url);
+
+}
---
+++ src/main/java/sample/service/WeatherServiceJsonOrg.java
@@ -0,0 +1,13 @@
+package sample.service;
+
+import sample.model.Weather;
+
+/**
+ * Created by Tomek on 2016-11-17.
+ */
+public class WeatherServiceJsonOrg implements WeatherService {
+
+    public Weather getWeatherFromUrl(String url) {
+        return null;
+    }
+}
